76cb655340af2e84af87d566e03530ccb5925fa0
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-31 23:18:52 -0500

    two ways to access form data on submit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const submitHandler = (event) => {
+  event.preventDefault();
+  console.log("The form was submitted");
+  // (one way to) Get the name input
+  /* const parkName = document.querySelector("#name-input").value;
+console.log(parkName); */
+  // (other way to access all form data)
+  const formData = new FormData(event.target);
+  const name = formData.get("name");
+  console.log(name);
+};
 
+const main = () => {
+  // Get the form element
+  const form = document.querySelector("#park-form");
+
+  // Attach the submit handler
+  form.addEventListener("submit", submitHandler);
+};
+
+//waits for html to load
+window.addEventListener("DOMContentLoaded", main);
